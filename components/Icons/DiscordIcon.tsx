39a4106e859c1446920b6c93585c090977a04806
Md Mohiuddin Khan Mahin <63153621+mahinkhan1408@users.noreply.github.com>
2025-11-19 18:42:24 +0600

    Add files via upload

---
+++ components/Icons/DiscordIcon.tsx
@@ -0,0 +1,22 @@
+
+import React from 'react';
+
+export const DiscordIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
+    <circle cx="9" cy="12" r="1"></circle>
+    <circle cx="15" cy="12" r="1"></circle>
+    <path d="M7.5 16C7.5 16 8.8 17 12 17C15.2 17 16.5 16 16.5 16"></path>
+    <path d="M15.5 21C15.5 21 16.5 18 16.5 18C19.5 17.5 20 14 20 14C20 14 18 12 18 12C18 12 16.5 13 16.5 13C15.5 12.5 14.5 12.5 12 12.5C9.5 12.5 8.5 12.5 7.5 13C7.5 13 6 12 6 12C6 12 4 14 4 14C4 14 4.5 17.5 7.5 18C7.5 18 8.5 21 8.5 21"></path>
+  </svg>
+);
